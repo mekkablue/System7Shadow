579c8a7d9154f4871318e924b38237bea77e81a0
schriftgestalt <georg.seifert@mac.com>
2024-09-27 22:50:25 +0200

    code cleanup

--- System7Shadow.glyphsFilter/Contents/Resources/plugin.py
+++ System7Shadow.glyphsFilter/Contents/Resources/plugin.py
@@ -16,36 +16,37 @@ from __future__ import division, print_function, unicode_literals
 ###########################################################################################################
 
 from __future__ import division, print_function, unicode_literals
-import objc, math
+import objc
+import math
 
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from GlyphsApp import Glyphs
+from GlyphsApp.plugins import FilterWithDialog
 from AppKit import NSAffineTransform, NSAffineTransformStruct
 from Foundation import NSClassFromString
 
-@objc.python_method
-def offsetLayer( thisLayer, offsetX, offsetY, makeStroke=False, position=0.5, autoStroke=False ):
+
+def offsetLayer(thisLayer, offsetX, offsetY, makeStroke=False, position=0.5, autoStroke=False):
 	offsetFilter = NSClassFromString("GlyphsFilterOffsetCurve")
 	try:
-		# GLYPHS 3:	
+		# GLYPHS 3:
 		offsetFilter.offsetLayer_offsetX_offsetY_makeStroke_autoStroke_position_metrics_error_shadow_capStyleStart_capStyleEnd_keepCompatibleOutlines_(
 			thisLayer,
-			offsetX, offsetY, # horizontal and vertical offset
-			makeStroke,     # if True, creates a stroke
-			autoStroke,     # if True, distorts resulting shape to vertical metrics
-			position,       # stroke distribution to the left and right, 0.5 = middle
-			None, None, None, 0, 0, False )
+			offsetX, offsetY,  # horizontal and vertical offset
+			makeStroke,        # if True, creates a stroke
+			autoStroke,        # if True, distorts resulting shape to vertical metrics
+			position,          # stroke distribution to the left and right, 0.5 = middle
+			None, None, None, 0, 0, False)
 	except:
 		# GLYPHS 2:
 		offsetFilter.offsetLayer_offsetX_offsetY_makeStroke_autoStroke_position_error_shadow_(
 			thisLayer,
-			offsetX, offsetY, # horizontal and vertical offset
-			makeStroke,     # if True, creates a stroke
-			autoStroke,     # if True, distorts resulting shape to vertical metrics
-			position,       # stroke distribution to the left and right, 0.5 = middle
-			None, None )
+			offsetX, offsetY,  # horizontal and vertical offset
+			makeStroke,        # if True, creates a stroke
+			autoStroke,        # if True, distorts resulting shape to vertical metrics
+			position,          # stroke distribution to the left and right, 0.5 = middle
+			None, None)
 
-@objc.python_method
+
 def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
 	"""
 	Returns an NSAffineTransform object for transforming layers.
@@ -66,7 +67,7 @@ def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
 	if scale != 1.0:
 		myTransform.scaleBy_(scale)
 	if not (shiftX == 0.0 and shiftY == 0.0):
-		myTransform.translateXBy_yBy_(shiftX,shiftY)
+		myTransform.translateXBy_yBy_(shiftX, shiftY)
 	if skew:
 		skewStruct = NSAffineTransformStruct()
 		skewStruct.m11 = 1.0
@@ -79,19 +80,19 @@ def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
 
 
 class System7Shadow(FilterWithDialog):
-	
+
 	# Definitions of IBOutlets
-	
+
 	# The NSView object from the User Interface. Keep this here!
 	dialog = objc.IBOutlet()
-	
+
 	# Text field in dialog
 	outlineXField = objc.IBOutlet()
 	outlineYField = objc.IBOutlet()
 	depthXField = objc.IBOutlet()
 	depthYField = objc.IBOutlet()
 	keepSidebearingsField = objc.IBOutlet()
-	
+
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
@@ -100,70 +101,70 @@ class System7Shadow(FilterWithDialog):
 			'fr': 'Ombre de Système 7',
 			'es': 'Ombra de la Sistema 7',
 		})
-		
+
 		# Word on Run Button (default: Apply)
 		self.actionButtonLabel = Glyphs.localize({
 			'en': u'Shadow',
 			'de': u'Schatten',
 		})
-		
+
 		# Load dialog from .nib (without .extension)
 		self.loadNib('IBdialog', __file__)
-	
+
 	# On dialog show
 	@objc.python_method
 	def start(self):
-		
+
 		# Set default value
 		Glyphs.registerDefault('com.mekkablue.system7shadow.outlineX', 80)
 		Glyphs.registerDefault('com.mekkablue.system7shadow.outlineY', 74)
 		Glyphs.registerDefault('com.mekkablue.system7shadow.depthX', 160)
 		Glyphs.registerDefault('com.mekkablue.system7shadow.depthY', 135)
 		Glyphs.registerDefault('com.mekkablue.system7shadow.keepSidebearings', 1)
-		
+
 		# Set value of text field
 		self.outlineXField.setStringValue_(Glyphs.defaults['com.mekkablue.system7shadow.outlineX'])
 		self.outlineYField.setStringValue_(Glyphs.defaults['com.mekkablue.system7shadow.outlineY'])
 		self.depthXField.setStringValue_(Glyphs.defaults['com.mekkablue.system7shadow.depthX'])
 		self.depthYField.setStringValue_(Glyphs.defaults['com.mekkablue.system7shadow.depthY'])
 		self.keepSidebearingsField.setState_(Glyphs.defaults['com.mekkablue.system7shadow.keepSidebearings'])
-		
+
 		# Set focus to text field
 		self.outlineXField.becomeFirstResponder()
-		
+
 	# Actions triggered by UI
 	@objc.IBAction
-	def setOutlineX_( self, sender ):
+	def setOutlineX_(self, sender):
 		Glyphs.defaults['com.mekkablue.system7shadow.outlineX'] = sender.floatValue()
 		self.update()
-	
+
 	@objc.IBAction
-	def setOutlineY_( self, sender ):
+	def setOutlineY_(self, sender):
 		Glyphs.defaults['com.mekkablue.system7shadow.outlineY'] = sender.floatValue()
 		self.update()
-	
+
 	@objc.IBAction
-	def setDepthX_( self, sender ):
+	def setDepthX_(self, sender):
 		Glyphs.defaults['com.mekkablue.system7shadow.depthX'] = sender.floatValue()
 		self.update()
-	
+
 	@objc.IBAction
-	def setDepthY_( self, sender ):
+	def setDepthY_(self, sender):
 		Glyphs.defaults['com.mekkablue.system7shadow.depthY'] = sender.floatValue()
 		self.update()
 
 	@objc.IBAction
-	def setKeepSidebearings_( self, sender ):
+	def setKeepSidebearings_(self, sender):
 		Glyphs.defaults['com.mekkablue.system7shadow.keepSidebearings'] = sender.state()
 		self.update()
-	
+
 	# Actual filter
 	@objc.python_method
 	def filter(self, layer, inEditView, customParameters):
 		# fallback values:
 		outlineX, outlineY, depthX, depthY = 80, 74, 160, 135
 		keepSidebearings = 1
-		
+
 		# Called on font export, get value from customParameters
 		if 'outlineX' in customParameters:
 			outlineX = customParameters['outlineX']
@@ -175,7 +176,7 @@ class System7Shadow(FilterWithDialog):
 			depthY = customParameters['depthY']
 		if 'keepSidebearings' in customParameters:
 			keepSidebearings = customParameters['keepSidebearings']
-		
+
 		# Called through UI, use stored value
 		else:
 			outlineX = float(Glyphs.defaults['com.mekkablue.system7shadow.outlineX'])
@@ -183,15 +184,15 @@ class System7Shadow(FilterWithDialog):
 			depthX = float(Glyphs.defaults['com.mekkablue.system7shadow.depthX'])
 			depthY = float(Glyphs.defaults['com.mekkablue.system7shadow.depthY'])
 			keepSidebearings = float(Glyphs.defaults['com.mekkablue.system7shadow.keepSidebearings'])
-		
+
 		fatLayer = layer.copyDecomposedLayer()
 		fatLayer.removeOverlap()
 		fatLayer.cleanUpPaths()
-		offsetLayer( fatLayer, outlineX, outlineY )
-		
-		for h,v in ((depthX,0), (0,depthY)):
+		offsetLayer(fatLayer, outlineX, outlineY)
+
+		for h, v in ((depthX, 0), (0, depthY)):
 			mergeLayer = fatLayer.copy()
-			shift = transform(h,-v)
+			shift = transform(h, -v)
 			shiftMatrix = shift.transformStruct()
 			mergeLayer.applyTransform(shiftMatrix)
 			for path in mergeLayer.paths:
@@ -202,17 +203,17 @@ class System7Shadow(FilterWithDialog):
 				except:
 					# GLYPHS 2
 					fatLayer.paths.append(mergePath)
-				
+
 			fatLayer.removeOverlap()
 			fatLayer.cleanUpPaths()
-		
+
 		originalLSB = layer.LSB
 		originalRSB = layer.RSB
-		
+
 		layer.decomposeComponents()
 		layer.removeOverlap()
 		layer.cleanUpPaths()
-		
+
 		for fatPath in fatLayer.paths:
 			try:
 				# GLYPHS 3
@@ -220,17 +221,15 @@ class System7Shadow(FilterWithDialog):
 			except:
 				# GLYPHS 2
 				layer.paths.append(fatPath.copy())
-			
-		
+
 		if keepSidebearings:
 			layer.LSB = originalLSB
 			layer.RSB = originalRSB
-		
+
 		layer.correctPathDirection()
-		
-	
+
 	@objc.python_method
-	def generateCustomParameter( self ):
+	def generateCustomParameter(self):
 		return "%s; outlineX:%s; outlineY:%s; depthX:%s; depthY:%s; keepSidebearings:%s" % (
 			self.__class__.__name__,
 			Glyphs.defaults['com.mekkablue.system7shadow.outlineX'],
@@ -238,8 +237,8 @@ class System7Shadow(FilterWithDialog):
 			Glyphs.defaults['com.mekkablue.system7shadow.depthX'],
 			Glyphs.defaults['com.mekkablue.system7shadow.depthY'],
 			Glyphs.defaults['com.mekkablue.system7shadow.keepSidebearings'],
-			)
-	
+		)
+
 	@objc.python_method
 	def __file__(self):
 		"""Please leave this method unchanged"""
